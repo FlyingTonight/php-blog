811e69fb7bfacb570a593cd63ccb33dd0d80416f
FlyingTonight <hikmasterr@gmail.com>
2023-03-31 11:22:01 +0500

    post-show

--- blog.php
+++ blog.php
@@ -42,14 +42,14 @@ $posts = $statement->fetchAll();
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
             <div class="card-body">
-              <h5><?php $post['title'] ?></h5>
-              <p class="card-text"><? $post['body'] ?></p>
+              <h5><?php echo $post['title'] ?></h5>
+              <p class="card-text"><? echo $post['body'] ?></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
-                <small class="text-body-secondary"><? $post['created_at'] ?></small>
+                <small class="text-body-secondary"><? echo $post['created_at'] ?></small>
               </div>
             </div>
           </div>
